0596a5f81604706707f72c70ecd2e0337674519b
johnnkp <johnnkp@hotmail.com>
2019-03-28 16:01:15 +0800

    Step 2: Pick one mathematics operator randomly

--- app/src/main/java/hkcc/ccn3165/assignment/maths/MainActivity.java
+++ app/src/main/java/hkcc/ccn3165/assignment/maths/MainActivity.java
@@ -2,6 +2,9 @@ package hkcc.ccn3165.assignment.maths;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView question = findViewById(R.id.question);
+        question.setText(question());
+    }
+
+    private String question() {
+        Random myRandom = new Random();
+        String mQuestion = "Question: ", mOperator, operator[] = {"+", "-", "×", "÷"};
+
+        mOperator = operator[myRandom.nextInt(4)];
+        mQuestion = mQuestion + mOperator;
+        return mQuestion;
     }
 }
