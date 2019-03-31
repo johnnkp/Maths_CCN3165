cb2438eef19da76cbc6c5af2cc388a769479b069
johnnkp <22496821+johnnkp@users.noreply.github.com>
2019-03-31 16:36:57 +0800

    Step 7: Summary of correctly answered questions

--- app/src/main/java/hkcc/ccn3165/assignment/maths/Summary.java
+++ app/src/main/java/hkcc/ccn3165/assignment/maths/Summary.java
@@ -2,6 +2,7 @@ package hkcc.ccn3165.assignment.maths;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        TextView mScore = findViewById(R.id.score);
+        String summaryText = "You have answered ", correct = " questions correctly.", incorrect = " questions wrongly.";
+        mScore.setText(summaryText + MainActivity.score + correct);
     }
 }
